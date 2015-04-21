8a12a04f86831d8e01aa3247c9b7a294244ca19e
LokeshSreekanta <lokesh.sreekanta@gmail.com>
2015-04-21 14:58:57 -0700

    Adding first android project

---
+++ AndroidStudioProjects/LifeCycleEvents/app/src/main/java/edu/sdsu/lokesh/lifecycleevents/LifeCycleEvents.java
@@ -0,0 +1,71 @@
+package edu.sdsu.lokesh.lifecycleevents;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class LifeCycleEvents extends ActionBarActivity {
+
+    private TextView textView;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle_events);
+        textView= (TextView)findViewById(R.id.textOnScreen);
+        textView.setText("onCreate\n");
+        Log.i("onCreate", "onCreate() called");
+    }
+
+     @Override
+    public void onStart() {
+        super.onStart();
+        textView.append("onStart\n");
+        Log.i("onStart", "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        textView.append("onResume\n");
+        Log.i("onResume", "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        textView.append("onPause\n");
+        Log.i("onPause","onPause() called");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        textView.append("onRestoreInstanceState\n");
+        Log.i("onRestoreInstanceState", "onRestoreInstanceState() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        textView.append("onSaveInstanceState\n");
+        Log.i("onSaveInstanceState", "onSaveInstanceState() called");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        textView.append("onRestart\n");
+        Log.i("onRestart", "onRestart() called");
+    }
+
+    public void clear(View clearButton) {
+        textView.setText(" ");
+
+    }
+}
